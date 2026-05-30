b64b26b0667e358a66a3aa2c2357e2bc62c65ca7
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 10:35:35 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class invoiceDetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = invoiceDetchWorker()
+    print("Worker engine initialized.")
